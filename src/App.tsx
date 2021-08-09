679cb34146ee96805bec0ecaa4e80220d9c95d0f
Clément Krajecki <clement.krajecki@gmail.com>
2021-08-09 10:30:30 +0200

    added a  footer

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,8 @@ const App = () => (
     <h3 className="bold webpage-title">À la une</h3>
     <RssContent {...{ rssFeeds }} />
     </div>
+    <footer> <div className="footer-container">
+    <h5 className="logo-text">nuntium</h5>   <div className="footnote">nuntium est le lecteur d'actualités le plus simple. Les actualités sont organisées par thème et les plus récentes sont mises en avant. Suivre les nouvelles en France et dans le monde entier n'a jamais été ausi facile.</div></div></footer>
   </div>
 );
 
