df67eb5ff3eee3409b62da930eda03ed52f7ee00
Clément Krajecki <clement.krajecki@gmail.com>
2021-08-14 12:23:58 +0200

    updated footnote

--- src/App.tsx
+++ src/App.tsx
@@ -16,8 +16,10 @@ const App = () => (
         <h5 className="logo-text">nuntium</h5>
         <div className="footnote v-separator">Version bêta du projet nuntium, pour un résumé des actualités intuitif, organisé de manière efficace en thèmes.</div>
         <div className="footnote inline">2021 - France</div>
-        <div className="footnote inline h-separator">Créé par <a className="color-link" href="https://www.reddit.com/user/iKL3W" target="_blank" rel="noreferrer">u/iKL3W</a>, avec l'aide de <a className="color-link" href="https://www.reddit.com/user/besthelloworld" target="_blank" rel="noreferrer">u/besthelloworld</a>.</div>
-        <div className="footnote inline"><b><a className="footnote-link" href="mailto:clement.krajecki@gmail.com?subject=nuntium&body=Bonjour!">Contact</a></b></div>
+        <div className="footnote inline">Créé par <a className="color-link" href="https://www.reddit.com/user/iKL3W" target="_blank" rel="noreferrer">u/iKL3W</a>, avec <a className="color-link" href="https://www.reddit.com/user/besthelloworld" target="_blank" rel="noreferrer">u/besthelloworld</a>.</div>
+        <div className="footnote"></div>
+        <div className="footnote inline h-separator"><b><a className="footnote-link" href="mailto:clement.krajecki@gmail.com?subject=nuntium&body=Bonjour!">Contact</a></b></div>
+        <div className="footnote inline"><b><a className="footnote-link" href="https://github.com/EthanStandel/browser-rss-parser">Github</a></b></div>
       </div>
     </footer>
   </div>
