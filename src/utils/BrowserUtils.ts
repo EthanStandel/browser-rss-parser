feba5961fbd30e991b23f804e5f8cb2de74bbb74
Ethan Standel <ethan.standel@rocketinsights.com>
2021-09-19 05:17:40 -0400

    Scroll header into view if available rather than true top of the container.

--- src/utils/BrowserUtils.ts
+++ src/utils/BrowserUtils.ts
@@ -1,9 +1,16 @@
 export const BrowserUtils = {
   scrollToTop: () => {
     try {
-      document.scrollingElement!.scrollTo({ top: 0, behavior: 'smooth' })
+      document.querySelector("h3")!.scrollIntoView();
+      document.scrollingElement!.scrollTop -= document.querySelector("header")!.offsetHeight
     } catch {
-      console.warn("Failed to scroll to top, no scrollingElement on document");
+      console.warn("Failed to scroll to top of header, using fallback scrolling method");
+
+      try {
+        document.querySelector("h3")!.scrollIntoView();
+      } catch {
+        console.log("Fallback scroll failed");
+      }
     }
   }
 }
