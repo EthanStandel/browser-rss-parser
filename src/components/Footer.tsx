bd36ecfbc3c26f663f5f9e654cb6b38f7e01da7c
Clément Krajecki <clement.krajecki@gmail.com>
2022-06-24 16:43:52 +0200

    Few adjustements for typgraphy styles.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,10 +4,10 @@ export const Footer = () =>  {
     <div className="footer-container">
       <section className="footer-header-app">
         <h1 className="logo-text">nuntium</h1>
-        <p className="h7">
+        <p className="h8">
           Une application pour faire un tour des actualités locales, nationales et internationales avec des articles de rédactions françaises, francophones et étrangères. 
         </p>
-        <p className="h7">
+        <p className="h8">
           Chaque sujet est traité en profondeur par les rédactions spécialisées pour une information de qualité. Les balados et autres émissions sont disponibles pour suivre l'actualité du globe à toute heure du jour et de la nuit.
         </p>
       </section>
@@ -17,7 +17,7 @@ export const Footer = () =>  {
           <p className="footnote inline">Créé par <a className="color" href="https://www.reddit.com/user/iKL3W" target="_blank" rel="noreferrer">u/iKL3W</a>, avec <a className="color" href="https://www.reddit.com/user/besthelloworld" target="_blank" rel="noreferrer">u/besthelloworld</a>.</p>
           <p className="footnote inline"><em>nuntium</em> n'est affilié ni à <a href="https://www.apple.com" target="_blank" rel="noreferrer">Apple</a>, ni à une quelconque rédaction.</p>
         </div>
-        <nav className="mini-links footnote bold">
+        <nav className="mini-links c1 bold">
           <a href="mailto:clement.krajecki@gmail.com?subject=nuntium&body=Bonjour!">Contact</a>
           <a href="https://github.com/EthanStandel/browser-rss-parser" target="_blank" rel="noreferrer">Github</a>
           <a href="http://atlasflux.saynete.net" target="_blank" rel="noreferrer">Atlas des flux</a>
--- src/components/HappyBirthday.tsx
+++ src/components/HappyBirthday.tsx
@@ -1,10 +1,8 @@
 export const HappyBday = () =>  {
-
-    return ( 
-               
+    return (           
       <div className="BlockParagraph Bday">
           <h2 className="logo-text">Happy Birthday!</h2>
-          <p className="h8">
+          <p>
             Le projet <em>nuntium</em> a été créeé le 16 juin 2021, nous fêtons donc ce mois-ci la première année de ce site web. Nous continuerons de développer ce projet, notamment avec l'ajout de podcasts d'information et de filtres plus personnalisés pour les utilisateurs dans les prochains mois. Nous espérons être une source fiable d'information chaque jour et pour tous ! 
           </p>
           <p className="footnote">
