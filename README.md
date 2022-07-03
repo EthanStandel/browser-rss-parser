43ee786496f1db7e8e793929f66008137bdb8b5d
Clément Krajecki <clement.krajecki@gmail.com>
2022-07-03 14:30:57 +0200

    Further rebranding into nuntii

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Browser RSS parser (rebranded nuntium)
+# Browser RSS parser (rebranded nuntii)
 
 I was asked to throw this together for someone.  It just utilizes a `rss_feeds.json` which should contain an `Array<{ url: string; name: string; ...}>` that defines RSS feeds.  I `fetch`* and parse those feeds and render some of their details to `<RssContent />`.
 
--- src/components/HappyBirthday.tsx
+++ src/components/HappyBirthday.tsx
@@ -3,7 +3,7 @@ export const HappyBday = () =>  {
       <div className="BlockParagraph Bday">
           <h2 className="logo-text">Happy Birthday!</h2>
           <p>
-            Le projet <em>nuntium</em> a été créeé le 16 juin 2021, nous fêtons donc ce mois-ci la première année de ce site web. Nous continuerons de développer ce projet, notamment avec l'ajout de podcasts d'information et de filtres plus personnalisés pour les utilisateurs dans les prochains mois. Nous espérons être une source fiable d'information chaque jour et pour tous ! 
+            Le projet <em>nuntii</em> a été créeé le 16 juin 2021, nous fêtons donc ce mois-ci la première année de ce site web. Nous continuerons de développer ce projet, notamment avec l'ajout de podcasts d'information et de filtres plus personnalisés pour les utilisateurs dans les prochains mois. Nous espérons être une source fiable d'information chaque jour et pour tous ! 
           </p>
           <p className="footnote">
             <em>KL3W</em>, fondateur du projet et développeur interface
